fecb1b0bba0ef028c625ba41c07bc4e0887ce1ee
Casper Chaithon Pedersen <casp6665@edu.easj.dk>
2018-09-17 19:00:24 +0200

    Add Test base and Bil classe

---
+++ BilletLibrary/Bil.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace BilletLibrary
+{
+    public class Bil
+    {
+        public string Nummerplade { get; set; }
+
+        public DateTime Dato { get; set; }
+
+
+        public Bil()
+        {
+            
+        }
+
+        public decimal Pris()
+        {
+            return 240;
+        }
+
+        public string Køretøj()
+        {
+            return "Bil";
+        }
+    }
+}
---
+++ UnitTestProject/UnitTest1.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using BilletLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void BilPris()
+        {
+            //Arange
+            var bil = new Bil();
+            //Act
+            decimal pris = bil.Pris();
+            //Assort
+            Assert.AreEqual(240,pris);
+        }
+
+        [TestMethod]
+        public void BilKøretøj()
+        {
+            //Arange
+            var bil = new Bil();
+            //Act
+            string køretøj = bil.Køretøj();
+            //Assort
+            Assert.AreEqual("Bil",køretøj);
+        }
+    }
+}
